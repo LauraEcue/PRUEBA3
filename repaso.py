4e062cecdac2d5a70add286b3d4d041ff76dca83
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-09-14 19:56:34 -0500

    feat

--- repaso.py
+++ repaso.py
@@ -1,15 +1,20 @@
+#primera forma
+
 #notas = [[5,4,3], [2,2,2], [1,1,1]] 
 #for nota in notas:
- #   print(nota)
+#    print(nota)
 
-estudiantes = [[5,4,3], [4,4,4], [3,3,3]]
-#i = [5,4,3]
-#for estudiantes in i:
-    #print (estudiantes)
+#==========================================
+
+#segunda forma
+#estudiantes = [[5,4,3], [4,4,4], [3,3,3]]
+#for estudiante in estudiantes:
+ #   print (estudiantes)
 #for nota in estudiantes:
-    #print (nota)
+ #   print (nota)
 
-
-for i in range (len(estudiantes)):
-    for j in range (len(estudiantes[i])):
-        print (estudiantes[i][j])
+#=========================================
+#tercera forma
+#for i in range (len(estudiantes)):
+ #   for j in range (len(estudiantes[i])):
+  #      print (estudiantes[i][j])
