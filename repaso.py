4653f946441dc2969a600c0e4cd1bf354e35e8a6
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-09-14 19:50:37 -0500

    feat

---
+++ repaso.py
@@ -0,0 +1,15 @@
+#notas = [[5,4,3], [2,2,2], [1,1,1]] 
+#for nota in notas:
+ #   print(nota)
+
+estudiantes = [[5,4,3], [4,4,4], [3,3,3]]
+#i = [5,4,3]
+#for estudiantes in i:
+    #print (estudiantes)
+#for nota in estudiantes:
+    #print (nota)
+
+
+for i in range (len(estudiantes)):
+    for j in range (len(estudiantes[i])):
+        print (estudiantes[i][j])
